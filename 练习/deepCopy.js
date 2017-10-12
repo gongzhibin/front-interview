aa40efeb1aec6021fd5edd254692197f1b10f484
gongzhibin <2271721552@qq.com>
2017-10-12 20:19:02 +0800

    滴滴面试--深拷贝

---
+++ 练习/deepCopy.js
@@ -0,0 +1,21 @@
+function deepCopy(obj,res){
+    var res = res || {};
+    for(var i in obj ){
+        if(typeof obj[i] === 'Object'){
+            res[i] = (obj[i].constructor === Array)? [] : {};
+            deepCopy(obj[i],res[i]);
+        }else{
+            res[i] = obj[i];
+        }
+    }
+    return res;
+}
+
+var a = {"name":123}
+
+var b = {"age":30,"obj":{
+    "test":123
+}}
+
+c = deepCopy(a,b);
+console.log(c.toString());

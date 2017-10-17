20ed297430b62d491434e3507a16167591e001f7
gongzhibin <2271721552@qq.com>
2017-10-17 22:18:08 +0800

    js枚举属性

---
+++ 练习/枚举.js
@@ -0,0 +1,23 @@
+// 'use strict';
+var obj = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+};
+Object.prototype.age = 24;
+Object.defineProperty(obj, "school", {
+    configurable: true,
+    writable: true,
+    //枚举的属性为enumerable
+    enumerable: true,
+    value: 'whut'
+})
+// var a = Object.keys(obj);
+for (var index in obj) {
+    if (obj.hasOwnProperty(index)) {
+        console.log(obj[index]);
+    }
+}
+// for (var value of obj) {
+//     console.log(value);
+// }

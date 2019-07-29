956adace1b99b6bb6e2058b470acd0ae82a2b5b9
billgong(巩志彬) <billgong@wesure.cn>
2019-07-29 12:53:05 +0800

    继续修改原型继承

--- 社招/interview/toutiao20190726/1.原型继承.js
+++ 社招/interview/toutiao20190726/1.原型继承.js
@@ -14,6 +14,11 @@ function Dog() {
     this.name = 'dog';
 };
 
+// function Dog() {
+//     Animal.apply(this, arguments); // apply super constructor.
+//     this.name = 'dog';
+// }
+
 
 // 1.1 new + constructor
 // 缺陷: 这种方式会将Animal实例中的属性添加到Dog的原型上
@@ -33,9 +38,7 @@ console.log(blackDog.name);
 console.log(blackDog.eat());
 
 // 1.2. __proto__
-// 缺陷:1.__proto__这个属性不应该被用户直接访问并修改
-//      2. 应该完全将其抛弃，因为这个行为完全不具备性能可言，许多浏览器优化了原型
-//      3.不支持 IE10 及以下的浏览器版本。
+// 缺陷:1.__proto__这个属性不应该被用户直接访问并修改；2.如果你在生产环境中使用这个方法，那么快速运行 Javascript 就是不可能的，因为许多浏览器优化了原型，尝试在调用实例之前猜测方法在内存中的位置，但是动态设置原型干扰了所有的优化，甚至可以强制一些浏览器重新编译来反优化您的代码，以使其根据规范工作；3.不支持 IE10 及以下的浏览器版本。
 Dog.prototype.__proto__ = Animal.prototype;
 
 const yellowDog = new Dog();
@@ -55,14 +58,20 @@ console.log(yellowDog instanceof Dog);
 console.log(yellowDog instanceof Animal);
 
 
-// 1.3 Object.create + constructor
-// 缺陷：1. 不支持 IE8 以下的版本;
-//      2.这个慢对象初始化在使用第二个参数的时候有可能成为一个性能黑洞，因为每个对象的描述符属性都有自己的描述对象。当以对象的格式处理成百上千的对象描述的时候，可能会造成严重的性能问题。
+// 1.3 Object.setPrototypeOf + constructor
+// 缺陷：这个方式表现并不好，应该被弃用（跟__proto__类似）。1.如果你在生产环境中使用这个方法，那么快速运行 Javascript 就是不可能的，因为许多浏览器优化了原型，尝试在调用实例之前猜测方法在内存中的位置，但是动态设置原型干扰了所有的优化，甚至可以强制一些浏览器重新编译来反优化您的代码，以使其根据规范工作。2.不支持 IE8 及以下的浏览器版本。
 
-function Dog() {
-    Animal.apply(this, arguments); // apply super constructor.
-    this.name = 'dog';
-}
+// Object.setPrototypeOf()方法设置一个指定的对象的原型 ( 即, 内部[[Prototype]]属性）到另一个对象或null。由于现代JavaScript引擎优化属性访问所带来的特性的关系，更改对象的[[Prototype]]在各个浏览器和 JavaScript 引擎上都是一个很慢的操作。其在更改继承的性能上的影响是微妙而又广泛的，这不仅仅限于obj.__proto__ = ... 语句上的时间花费，而且可能会延伸到任何代码，那些可以访问任何[[Prototype]]已被更改的对象的代码。如果你关心性能，你应该避免设置一个对象的[[Prototype]]。相反，你应该使用Object.create()来创建带有你想要的[[Prototype]]的新对象。
+
+Dog.prototype = Object.setPrototypeOf({}, Animal.prototype);
+Object.defineProperty(Dog.prototype, 'constructor', {
+    enumerable: false,
+    value: Dog
+});
+
+
+// 1.4 Object.create + constructor
+// 缺陷：1. 不支持 IE8 以下的版本; 2.这个慢对象初始化在使用第二个参数的时候有可能成为一个性能黑洞，因为每个对象的描述符属性都有自己的描述对象。当以对象的格式处理成百上千的对象描述的时候，可能会造成严重的性能问题。
 
 Dog.prototype = Object.create(Animal.prototype);
 Object.defineProperty(Dog.prototype, 'constructor', {
@@ -70,13 +79,6 @@ Object.defineProperty(Dog.prototype, 'constructor', {
     value: Dog
 });
 
-// 1.4 Object.setPrototypeOf + constructor
-// 缺陷：这个方式表现并不好，应该被弃用。1.如果你在生产环境中使用这个方法，那么快速运行 Javascript 就是不可能的，因为许多浏览器优化了原型，尝试在调用实例之前猜测方法在内存中的位置，但是动态设置原型干扰了所有的优化，甚至可能使浏览器为了运行成功，使用完全未经优化的代码进行重编译。2.不支持 IE8 及以下的浏览器版本。
-Dog.prototype = Object.setPrototypeOf({}, Animal.prototype);
-Object.defineProperty(Dog.prototype, 'constructor', {
-    enumerable: false,
-    value: Dog
-});
 
 // 1.5 中介 + new + constructor
 function F() {}
@@ -100,6 +102,7 @@ function extend(Child, Parent) {
 
 // 实例的原型指向构造函数的原型对象
 new Dog().__proto__ === Dog.prototype;
+
 
 // 1.6. class extends
 class Person {

f4c147ba23f6e3b5c77901aec1df75a4dd9fd7d7
billgong(巩志彬) <billgong@wesure.cn>
2019-08-05 14:16:53 +0800

    change style

--- JS/deepClone/deepCopy.js
+++ JS/deepClone/deepCopy.js
@@ -1,21 +1,20 @@
-function deepCopy(obj,res){
-    var res = res || {};
-    for(var i in obj ){
-        if(typeof obj[i] === 'Object'){
-            res[i] = (obj[i].constructor === Array)? [] : {};
-            deepCopy(obj[i],res[i]);
-        }else{
-            res[i] = obj[i];
+function deepCopy(obj, res = {}){
+    for(let key in obj ){
+        if(typeof obj[key] === 'object'){
+            res[key] = (obj[key].constructor === Array)? [] : {};
+            deepCopy(obj[key], res[key]);
+        } else {
+            res[key] = obj[key];
         }
     }
     return res;
 }
 
-var a = {'name':123};
+const a = {'name': 123};
 
-var b = {'age':30,'obj':{
+const b = {'age': 30, 'obj' : {
     'test':123
 }};
 
-c = deepCopy(a,b);
+const c = deepCopy(a, b);
 console.log(c.toString());
